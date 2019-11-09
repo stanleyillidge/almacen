5563d4f5ca896e46f21f266e28e23cbec9702ee4
stanleyillidge <stanley.illidge@gmail.com>
2019-11-09 17:51:18 -0500

    fix webview error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,33 @@ import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { DataService } from './models/data-service';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import * as firebase from 'firebase/app';
+
+import { IonicStorageModule } from '@ionic/storage';
+import { Camera } from '@ionic-native/camera/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { ImageResizer } from '@ionic-native/image-resizer/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD-eQ5Z1rxGxJ0eSg_DUUr-G1_lggKVLfw",
+  authDomain: "almacen-develop.firebaseapp.com",
+  databaseURL: "https://almacen-develop.firebaseio.com",
+  projectId: "almacen-develop",
+  storageBucket: "almacen-develop.appspot.com",
+  messagingSenderId: "361786075749",
+  appId: "1:361786075749:web:6143f95df6dd9838dd748c",
+  measurementId: "G-V3824B8EKJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -18,13 +45,23 @@ import { DataService } from './models/data-service';
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    IonicStorageModule.forRoot()
   ],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    DataService
+    DataService,
+    Camera,
+    FileTransfer,
+    File,
+    ImageResizer,
+    WebView
   ],
   bootstrap: [AppComponent]
 })
--- src/app/models/data-service.ts
+++ src/app/models/data-service.ts
@@ -1,25 +1,25 @@
 import { Injectable, NgZone } from '@angular/core';
-// import * as firebase from 'firebase/app';
-// import 'firebase/auth';
-// import 'firebase/database';
-// import 'firebase/functions';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/functions';
 import { Router } from "@angular/router";
 import { LoadingController, AlertController, Platform, ToastController } from '@ionic/angular';
 // Ionic Storage
-// import { Storage } from '@ionic/storage';
+import { Storage } from '@ionic/storage';
 import { ReplaySubject } from 'rxjs';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 import { LocalDatabase } from './data-models';
-// import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
-// import { File } from '@ionic-native/file/ngx';
-// import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @Injectable()
 export class DataService {
-    // public SedesObserver: ReplaySubject<any> = new ReplaySubject<any>();
-    // public UbicacionesObserver: ReplaySubject<any> = new ReplaySubject<any>();
-    // public SubUbicacionesObserver: ReplaySubject<any> = new ReplaySubject<any>();
-    // public ArticuloBaseObserver: ReplaySubject<any> = new ReplaySubject<any>();
-    // public ArticulosObserver: ReplaySubject<any> = new ReplaySubject<any>();
+    public SedesObserver: ReplaySubject<any> = new ReplaySubject<any>();
+    public UbicacionesObserver: ReplaySubject<any> = new ReplaySubject<any>();
+    public SubUbicacionesObserver: ReplaySubject<any> = new ReplaySubject<any>();
+    public ArticuloBaseObserver: ReplaySubject<any> = new ReplaySubject<any>();
+    public ArticulosObserver: ReplaySubject<any> = new ReplaySubject<any>();
     database: LocalDatabase;
     plataforma: any = {desktop:Boolean,android:Boolean};
     looper:number = 0;
@@ -30,13 +30,14 @@ export class DataService {
         public router: Router,
         public loadingController: LoadingController,
         public ngZone: NgZone, // NgZone service to remove outside scope warning
-        // private webview: WebView,
-        // private fileTransfer: FileTransfer,
-        // private file: File,
-        // private storage: Storage
+        private webview: WebView,
+        private fileTransfer: FileTransfer,
+        private file: File,
+        private storage: Storage
     ) {
         let este = this;
         // console.log(this.platform)
+        // this.webview.convertFileSrc('')
         this.plataforma.desktop = this.platform.is("desktop");
         this.plataforma.android = this.platform.is("android");
         this.plataforma.cordova = this.platform.is("cordova");

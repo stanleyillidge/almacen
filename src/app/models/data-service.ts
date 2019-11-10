144cfedde5ff44aa9dfc0287357e61c8e2881410
jamezj007 <57469765+jamezj007@users.noreply.github.com>
2019-11-09 21:54:07 -0500

    nuevo cambio

--- src/app/models/data-service.ts
+++ src/app/models/data-service.ts
@@ -11,7 +11,7 @@ import { ReplaySubject } from 'rxjs';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { File } from '@ionic-native/file/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { LocalDatabase, Producto } from './data-models'; // hola
+import { LocalDatabase, Producto } from './data-models'; // hola stanley
 
 @Injectable()
 export class DataService {
